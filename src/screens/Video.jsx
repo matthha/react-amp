78211095ff1c7a1eeb6b50d1654d0a90bd8ee4e6
charlottedyt <33757670+charlottedyyt@users.noreply.github.com>
2024-03-19 11:57:14 -0400

    updated Quiz and Video pages' nav to use the components

--- src/screens/Video.jsx
+++ src/screens/Video.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import NavBar from "../ui-components/NavBar";
 import { useNavigate, useLocation } from "react-router-dom"; // Import useNavigate
 import {
   Drawer,
@@ -26,35 +26,7 @@ const IntroductionVideo = () => {
   };
   return (
     <div style={{ height: "100vh", backgroundColor: "whit" }}>
-      <div
-        style={{
-          background: "#9E2A2B",
-          height: "auto",
-          padding: "24px 16px",
-        }}
-        className="menuicon"
-      >
-        <MenuOutlined
-          style={{ color: "white", fontSize: 20 }}
-          onClick={() => {
-            setOpenMenu(true);
-          }}
-        />
-      </div>
-      <span className="headerMenu">
-        <AppMenu />
-      </span>
-      <Drawer
-        placement="left"
-        visible={openMenu}
-        onClose={() => {
-          setOpenMenu(false);
-        }}
-        closable={false}
-        style={{ backgroundColor: "white" }}
-      >
-        <AppMenu isInline={true} />
-      </Drawer>
+      <NavBar />
       <div style={{ background: "white", padding: 20, minHeight: "100vh" }}>
         <h1 className="header1">Introduction Video</h1>
         <div className="secondaryContent" style={{ padding: "12px 0px" }}>
@@ -88,40 +60,5 @@ const IntroductionVideo = () => {
     </div>
   );
 };
-
-function AppMenu({ isInline = false }) {
-  // Corrected parameter destructuring
-  const navigate = useNavigate();
-  const handleClick = (e) => {
-    // navigate to the route corresponding to the menu item key
-    navigate(`/${e.key}`);
-  };
- 
-  return (
-    <Menu
-      style={{ backgroundColor: "white", fontSize: 20, border: "none" }}
-      mode={isInline ? "inline" : "horizontal"}
-      onClick={handleClick}
-      items={[
-        {
-          label: "CCHS Online Orientation",
-          key: "home", // Corrected property name to lowercase
-        },
-        {
-          label: "Orientation Recap",
-          key: "recap",
-        },
-        {
-          label: "FAQ",
-          key: "faq",
-        },
-        {
-          label: "Profile",
-          key: "profile",
-        },
-      ]}
-    ></Menu>
-  );
-}
 
 export default IntroductionVideo;
