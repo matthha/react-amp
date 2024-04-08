a30430b9e98edccf0409d094ddc8731e60302476
Matthew <matthha@umich.edu>
2024-04-08 11:12:36 -0400

    Update Quiz Content

--- src/JSONs/Modules.js
+++ src/JSONs/Modules.js
@@ -4,13 +4,13 @@ export const content = [
     estimationTime: "~10 mins",
     coverImg:
       "https://cityhigh.org/files/content/Orientation/ScheduleAttendance.jpg",
-    videoLink: "https://www.youtube.com/embed/OQdwqhMClTI",
+    videoLink: "https://www.youtube.com/embed/Du-E0dQE6gM?si=eVryxRspxUF6tOQp",
     description: "Understand how abscences contribute to truancy.",
     content: [
-      {regular:"This is the first sentence."},
-      {bold:"This is a bolded sentence."},
-      {underline:"Look at me, I'm underlined!"},
-      {danger:"I'm very important."}
+      {regular:"City High School has a strict attendance policy. A student is considered truant with their 3rd unexcused absence in a year. Once truant they will have to attend Student Attendance Improvement Conference and repercussions will become more severe with more absences."},
+      {bold:"Legal Excuses do not count towards unexcused absences limit. These include things like Illness, Family Emergency, Medical Appointments, Legal/Court Appointments, Religious Holidays, Bereavement or Principal approved reasons."},
+      {underline:"The following may count towards their truancy absences unless a parent note is submitted within 3 days or it will be unexcused: Oversleeping, Missing the bus, Confusion about whether or not there was school, Vacations, Parent/Sibling illness, other responsibilities like work or childcare."},
+      {danger:"A student may only have 7 unexcused absences before they fail a class. Family vacation is unexcused and counts towards this limit."}
     ],
     quizList: [
       {
@@ -20,7 +20,7 @@ export const content = [
           "3rd unexcused absence",
           "6th absence",
         ],
-        correctAnswer: "5th unexcused absence",
+        correctAnswer: "3rd unexcused absence",
       },
       {
         question: "Who can excuse a student for being absent?",
--- src/ui-components/Content.jsx
+++ src/ui-components/Content.jsx
@@ -1,15 +1,15 @@
 const Content = (props) => {
 
    return (
-      <div >
+      <div className="content" >
       {props?.content.map((x,i)=> {
          return (
-         <>
+         <span key={i}>
             {x?.regular ?? <p>{x.regular}</p>}
-            {x?.bold ?? <h1 className="header1">{x.bold}</h1>}
+            {x?.bold ?? <h1 className="header4">{x.bold}</h1>}
             {x?.underline ?? <div style={{ color: "#338888" }}>{x.underline}</div>}
-            {x?.danger ?? <p style={{ color: "#338888" }}>{x.danger}</p>} 
-         </>
+            {x?.danger ?? <p style={{ color: "#999999" }}>{x.danger}</p>} 
+         </span>
          )  
       }
       )}
