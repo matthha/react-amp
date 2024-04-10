82bb25c772f17d11b053aa2c18eaad8287a32a78
Zixi Zhou <112566694+Zixiiii@users.noreply.github.com>
2024-04-11 02:06:11 +0800

    Add files via upload

--- src/screens/ProfilePage.jsx
+++ src/screens/ProfilePage.jsx
@@ -15,16 +15,22 @@ import {
   Row,
   Col,
 } from "antd";
+import { content } from "../JSONs/Modules";
+import { useProgress } from "../ProgressContext";
+
 const { Meta } = Card;
 // Import other icons here if needed and use them as intended
 
 
-
+const orientationModules = content;
 function ProfilePage(props) {
   const [openMenu, setOpenMenu] = useState(false);
   const [username, setUserName] = useState('');
   const [email, setUserEmail] = useState('');
-
+  const { completedModules, updateCompletedModules } = useProgress();
+  const totalModules = orientationModules.length;
+  const completionPercentage = Math.round(completedModules.length/totalModules * 100);
+  console.log(completionPercentage)
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
@@ -45,24 +51,46 @@ function ProfilePage(props) {
       <div className="contentBody">
       <div style={{ background: "white", padding: 20, minHeight: "100vh" }}>
         <h1 className="header1">Profile</h1>
-        <h3 className="header3" style={{paddingTop:25}}>GENERAL INFORMATION</h3>
-        <div style={{paddingTop:25}}>
-            <div>
-                <h4 className="header4" style={{paddingBottom:5}}>NAME</h4>
-                <input 
-                    defaultValue={username}
-                    className="profile-input" 
-                />
-            </div>
-            <div>
-                <h4 className="header4" style={{paddingBottom:5}}>EMAIL</h4>
-                <input 
-                    defaultValue={email}
-                    className="profile-input" 
-                />
+        <div>
+          <h3 className="header3" style={{paddingTop:25}}>GENERAL INFORMATION</h3>
+          <div style={{paddingTop:25}}>
+              <div>
+                  <h4 className="header4" style={{paddingBottom:5}}>NAME</h4>
+                  <input 
+                      defaultValue={username}
+                      className="profile-input" 
+                  />
+              </div>
+              <div>
+                  <h4 className="header4" style={{paddingBottom:5}}>EMAIL</h4>
+                  <input 
+                      defaultValue={email}
+                      className="profile-input" 
+                  />
+              </div>
+          </div>
+        </div>
+        <div>
+          <h3 className="header3" style={{paddingTop:25}}>COMPLETION STATUS</h3>
+          <div style={{paddingTop:25}}>
+              {completionPercentage >= 100 ? (
+                <div>
+                  <CheckCircleFilled style={{ color: "#299E63", fontSize: "24px" }} />
+                  <span style={{ marginLeft: "10px" }}>Completed</span>
+                  <div>
+                    <img src="https://media.istockphoto.com/id/1353254487/vector/complete-stamp-imprint-seal-template-vector-stock-illustration.jpg?s=612x612&w=0&k=20&c=WTGSZJSVsMoSyge9UhZz71HQoU0TB4ugcV4BSXtPYWU=" alt="Completed" 
+                    style={{ width: "180px", height: "150px" }} />
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <MinusCircleFilled style={{ color: "#C51C00", fontSize: "24px" }} />
+                  <span style={{ marginLeft: "10px" }}>Incomplete</span>
+                </div>
+              )}
             </div>
         </div>
-        <div style={{paddingTop:'40px'}}>
+        <div style={{paddingTop:'30px'}}>
           <button style={{
               backgroundColor: "#9E2A2B",
               border: "none",
