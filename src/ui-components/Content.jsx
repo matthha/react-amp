d1f8161b7a8a9324662fdb842687aa6df2027675
charlottedyt <33757670+charlottedyyt@users.noreply.github.com>
2024-04-16 11:37:44 -0400

    removed <reg> to avoid browser error and matches the styles

--- src/ui-components/Content.jsx
+++ src/ui-components/Content.jsx
@@ -1,20 +1,20 @@
-const Content = (props) => {
-   return (
-     <reg className="content">
-       {props?.content.map((x, i) => (
-         <reg key={i}>
-           {x.regular && <>{x.regular} </>}
-           {x.bold && <b style={{fontWeight: 600}}>{x.bold} </b>}
-           {x.underline && <reg style={{ textDecoration: "underline"}}>{x.underline} </reg>}
-           {x.link && <a href={x.link}>{x.link} </a>}
-           {x.danger && <reg style={{ fontWeight: "bold", color: "#FF0000", backgroundColor: "#FFE0E0", padding: "5px", borderRadius: "5px" }}>{x.danger} </reg>}
-           {x.break && <br></br>}
-           {x.tab && <pre style={{margin:0, display:"inline",paddingLeft:'1.5em'}}></pre>}
-         </reg>
-       ))}
-     </reg>
-   );
- };
+ const Content = (props) => {
+  return (
+    <div className="content"> {/* Adjust the base font size and line height as needed */}
+      {props?.content.map((x, i) => (
+        <span key={i} style={{ display: 'inline'}}> {/* Inline display with custom margin */}
+          {x.regular && <span>{x.regular} </span>}
+          {x.bold && <span style={{ fontWeight: 600 }}>{x.bold} </span>}
+          {x.underline && <span style={{ textDecoration: "underline" }}>{x.underline} </span>}
+          {x.link && <a href={x.link}>{x.link} </a>} {/* Style links explicitly */}
+          {x.danger && <span style={{ fontWeight: "bold", color: "#FF0000", backgroundColor: "#FFE0E0", padding: "5px", borderRadius: "5px" }}>{x.danger} </span>}
+          {x.break && <br />}
+          {x.tab && <span style={{margin:0, display:"inline",paddingLeft:'1.5em'}} />} {/* Use inline-block for tab spaces */}
+        </span>
+      ))}
+    </div>
+  );
+};
  
  export default Content;
  
