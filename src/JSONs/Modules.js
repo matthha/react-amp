5c1dbd07733bacd4eb9644d63a6301694f89534c
Matthew <matthha@umich.edu>
2024-04-16 10:37:53 -0400

    Update Transportation Module Content

--- src/JSONs/Modules.js
+++ src/JSONs/Modules.js
@@ -159,13 +159,28 @@ export const content = [
     coverImg:
       "https://cityhigh.org/files/content/Orientation/TRANSPORTATION.jpg",
     videoLink: "https://www.youtube.com/watch?v=OQdwqhMClTI", //TODO change the example here later
-    description: "Know what students are expected to wear every day.",
+    description: "Understand transportation to and from school.",
     content: [
-      {regular:"This is the first sentence."},
-      {bold:"This is a bolded sentence."},
-      {underline:"Look at me, I'm underlined!"},
-      {danger:"I'm very important."}
-    ],
+      {tab:true}, {regular:"School districts provide transportation for students who live at least two miles but less than 10 miles from City Charter High School."},
+      {regular:"Every student is provided with transportation according to the rules set by their school district of residence. The type of transportation is determined by the home school district. Parents who would like additional transportation information must contact their school district of residence."},
+      {regular:"Any district that is further than 10 miles from City Charter High School has the right to refuse transportation, and the family will be responsible for transportation."},
+      {break:true},
+      {bold:"Student Transportation Managed By City High:"},
+      {break:true},
+      {tab:true},{regular:"City High manages the following districts’ Connect Cards, although it is the district's decision of whether monetary value, a weekly pass or a monthly pass is placed on the card."},
+      {danger:"In the event of a lost/stolen card, a replacement card will be issued at the cost of $5.00 cash when the new card is received.  It may take at least two weeks for a new card to be processed."},
+      {regular:" Lost/stolen cards should be reported to transportation@cityhigh.org."},
+      {list:[]},
+      {break:true},
+      {bold:"District-Provided Passes:"},
+      {break:true},
+      {tab:true},{regular:"The following school districts manage their own transportation.  Students who are residents of these districts will be given a bus pass by their districts, provided that they follow the district’s registration policy."},
+      {danger:"Please be advised that City High does not purchase, or replace lost, bus passes for students who reside within the below districts."},
+      {regular:"Students MUST contact their home district for transportation issues (i.e., lost cards, cards not working, etc.)"},
+      {list:[]},
+      {break:true},{bold:"New Address:"},{break:true},
+      {tab:true},{regular:"If you move, you must fill out a change of address form and submit 2 new proofs of residency within 10 days of the move.  If your school district has changed, you must register the new address with the new district. Please note that changing school districts can impact student transportation."}
+      ],
     quizList: [
       {
         question: "What is type of clothing is required at school?",
