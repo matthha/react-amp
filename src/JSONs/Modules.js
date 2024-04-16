71bfb57ad21f9ac6ba354e47fcacc97674533a60
Matthew <matthha@umich.edu>
2024-04-16 08:32:53 -0400

    Update styling for content

--- src/JSONs/Modules.js
+++ src/JSONs/Modules.js
@@ -7,11 +7,20 @@ export const content = [
     videoLink: "https://www.youtube.com/embed/Du-E0dQE6gM?si=eVryxRspxUF6tOQp",
     description: "Understand how abscences contribute to truancy.",
     content: [
-      {regular:"City High School has a strict attendance policy. A student is considered truant with their 3rd unexcused absence in a year. Once truant they will have to attend Student Attendance Improvement Conference and repercussions will become more severe with more absences."},
-      {bold:"Legal Excuses do not count towards unexcused absences limit. These include things like Illness, Family Emergency, Medical Appointments, Legal/Court Appointments, Religious Holidays, Bereavement or Principal approved reasons."},
+      {tab:true},
+      {regular:"City High School has a strict attendance policy unlike other schools. A student is considered truant with their 3rd unexcused absence in a year. Once truant they will have to attend Student Attendance Improvement Conference and repercussions will become more severe with more absences."},
+      {bold:"Truancy starts at the 3rd unexcussed absence."}, 
+      {regular:"It is important to avoid truancy."},
+      {break:true},
+      {tab:true}, 
+      {bold:"Legal Excuses do not count towards unexcused absences limit. These include things like Illness, Family Emergency, Medical Appointments, Legal/Court Appointments, Religious Holidays, Bereavement or Principal approved reasons. "},
       {link:"https://www.cityhigh.org/"},
-      {underline:"The following may count towards their truancy absences unless a parent note is submitted within 3 days or it will be unexcused: Oversleeping, Missing the bus, Confusion about whether or not there was school, Vacations, Parent/Sibling illness, other responsibilities like work or childcare."},
-      {danger:"A student may only have 7 unexcused absences before they fail a class. Family vacation is unexcused and counts towards this limit."}
+      {break:true},
+      
+      {break:true},
+      {tab:true},
+      {underline:"The following may count towards their truancy absences unless a parent note is submitted within 3 days or it will be unexcused: Oversleeping, Missing the bus, Confusion about whether or not there was school, Vacations, Parent/Sibling illness, other responsibilities like work or childcare. "},
+      {danger:"A student may only have 7 unexcused absences before they fail a class. Family vacation is unexcused and counts towards this limit. "}
     ],
     quizList: [
       {
--- src/ui-components/Content.jsx
+++ src/ui-components/Content.jsx
@@ -1,16 +1,18 @@
 const Content = (props) => {
    return (
-     <div className="content contentBox">
+     <reg className="content">
        {props?.content.map((x, i) => (
-         <span key={i}>
-           {x.regular && <p>{x.regular}</p>}
-           {x.bold && <p style={{fontWeight: 600}}>{x.bold}</p>}
-           {x.underline && <p style={{ textDecoration: "underline"}}>{x.underline}</p>}
-           {x.link && <a href={x.link}>{x.link}</a>}
-           {x.danger && <p style={{ fontWeight: "bold", color: "#FF0000", backgroundColor: "#FFE0E0", padding: "5px", borderRadius: "5px" }}>{x.danger}</p>}
-         </span>
+         <reg key={i}>
+           {x.regular && <>{x.regular} </>}
+           {x.bold && <b style={{fontWeight: 600}}>{x.bold} </b>}
+           {x.underline && <reg style={{ textDecoration: "underline"}}>{x.underline} </reg>}
+           {x.link && <a href={x.link}>{x.link} </a>}
+           {x.danger && <reg style={{ fontWeight: "bold", color: "#FF0000", backgroundColor: "#FFE0E0", padding: "5px", borderRadius: "5px" }}>{x.danger} </reg>}
+           {x.break && <br></br>}
+           {x.tab && <pre style={{margin:0, display:"inline",paddingLeft:'1.5em'}}></pre>}
+         </reg>
        ))}
-     </div>
+     </reg>
    );
  };
  
