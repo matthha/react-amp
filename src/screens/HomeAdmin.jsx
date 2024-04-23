aa24260f24e32acaa6d58b261b3631dfbf741f14
charlottedyt <33757670+charlottedyyt@users.noreply.github.com>
2024-04-22 23:25:49 -0400

    removed redundant styles

--- src/screens/HomeAdmin.jsx
+++ src/screens/HomeAdmin.jsx
@@ -26,31 +26,31 @@ function HomeAdmin(props) {
       //filter out if this is admin's progress
       return Number(progress) === 100 && obj.userID !== adminInfo.adminUsername;
     }
-  
+
     function CheckInProgress(obj) {
       let progress = (JSON.parse(obj?.progress).length / totalModules) * 100;
       //filter out if this is admin's progress
       return Number(progress) !== 100 && obj.userID !== adminInfo.adminUsername;
     }
-  
+
     function calculateProgress(obj) {
       return (JSON.parse(obj?.progress).length / totalModules) * 100;
     }
-  
+
     // Try to get the progress
     try {
       const apiData = await client.graphql({ query: listProgresses });
-  
+
       if (apiData.data.listProgresses.items.length !== 0) {
         setMyRecords(apiData.data.listProgresses.items);
-  
+
         const comp = apiData.data.listProgresses.items.filter(CheckFinished);
         const prog = apiData.data.listProgresses.items.filter(CheckInProgress);
-  
+
         // Sorting based on progress
         comp.sort((a, b) => calculateProgress(a) - calculateProgress(b));
         prog.sort((a, b) => calculateProgress(a) - calculateProgress(b));
-  
+
         setThePassed(comp);
         setTheInProgress(prog);
       }
@@ -58,7 +58,6 @@ function HomeAdmin(props) {
       console.log(error);
     }
   }
-  
 
   useEffect(() => {
     fetchProgress();
@@ -72,58 +71,54 @@ function HomeAdmin(props) {
     <div style={{ height: "100vh", backgroundColor: "white" }}>
       <NavBar />
       <div className="contentBody">
-        <div style={{ background: "white", padding: 20, minHeight: "100vh" }}>
-          <h1 className="header1">Orientation Dashboard</h1>
+        <h1 className="header1">Orientation Dashboard</h1>
 
-          <div className="content paragraph">
-            Hello, {props?.user.username} <br />
-            Current user progress are below:
-          </div>
-          <Tabs
-            defaultActiveKey="1"
-            className="content"
-            items={[
-              {
-                key: "1",
-                label: `In Progress (${theInProgress.length})`,
-                children: theInProgress.map((person, index) => (
-                  <div key={index} className="content">
-                    <h3>{person.userID}</h3>
-                    <p>
-                      Completed {JSON.parse(person?.progress).length} out of{" "}
-                      {totalModules}
-                    </p>
-                    <Progress
-                      percent={Math.round(
-                        (JSON.parse(person?.progress).length / totalModules) *
-                          100
-                      )}
-                    />
-                  </div>
-                )),
-              },
-              {
-                key: "2",
-                label: `Completed (${thePassed.length})`,
-                children: thePassed.map((person, index) => (
-                  <div key={index} className="content">
-                    <h3>{person.userID}</h3>
-                    <p>
-                      Completed {JSON.parse(person?.progress).length} out of{" "}
-                      {totalModules}
-                    </p>
-                    <Progress
-                      percent={Math.round(
-                        (JSON.parse(person?.progress).length / totalModules) *
-                          100
-                      )}
-                    />
-                  </div>
-                )),
-              },
-            ]}
-          />
+        <div className="content paragraph">
+          Hello, {props?.user.username} <br />
+          Current user progress are below:
         </div>
+        <Tabs
+          defaultActiveKey="1"
+          className="content"
+          items={[
+            {
+              key: "1",
+              label: `In Progress (${theInProgress.length})`,
+              children: theInProgress.map((person, index) => (
+                <div key={index} className="content">
+                  <h3>{person.userID}</h3>
+                  <p>
+                    Completed {JSON.parse(person?.progress).length} out of{" "}
+                    {totalModules}
+                  </p>
+                  <Progress
+                    percent={Math.round(
+                      (JSON.parse(person?.progress).length / totalModules) * 100
+                    )}
+                  />
+                </div>
+              )),
+            },
+            {
+              key: "2",
+              label: `Completed (${thePassed.length})`,
+              children: thePassed.map((person, index) => (
+                <div key={index} className="content">
+                  <h3>{person.userID}</h3>
+                  <p>
+                    Completed {JSON.parse(person?.progress).length} out of{" "}
+                    {totalModules}
+                  </p>
+                  <Progress
+                    percent={Math.round(
+                      (JSON.parse(person?.progress).length / totalModules) * 100
+                    )}
+                  />
+                </div>
+              )),
+            },
+          ]}
+        />
       </div>
     </div>
   );
