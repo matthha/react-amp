ed9fffd1ddd491b47ab06cc955e3a53cc52a5d49
charlottedyt <33757670+charlottedyyt@users.noreply.github.com>
2024-04-24 18:40:45 -0400

    Quiz page: Finished cleaning up code and added comments

--- src/screens/Quiz.jsx
+++ src/screens/Quiz.jsx
@@ -1,77 +1,71 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom"; // Import useNavigate
+import { useNavigate, useLocation } from "react-router-dom";
 import { useProgress } from "../ProgressContext";
 import NavBar from "../components/NavBar";
 import { Card, Radio, Button, Space, ConfigProvider } from "antd";
 import { generateClient } from "aws-amplify/api";
 import { updateProgress } from "../graphql/mutations";
 
-const Quiz = (props) => {
+const Quiz = () => {
   const { completedModules, updateCompletedModules } = useProgress();
   const [answers, setAnswers] = useState({});
   const location = useLocation();
+  const navigate = useNavigate();
+
+  // Scroll to the top of the window when the component mounts
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  const client = generateClient();
-  const variables = {
-    filter: {
-      userID: { eq: props.user.username },
-    },
-  };
-  const moduleName = location.state.module.title;
-  const module = location.state.module;
-  const myRecord = location.state.myRecord;
 
-  const navigate = useNavigate();
+  const client = generateClient(); // Initialize AWS Amplify client
+  const { module, myRecord } = location.state;
+  const { title: moduleName } = module;
 
+  // Handler to update progress in the backend
   async function updateOurProgress(newProgress) {
     try {
-      const updatedProgress = await client.graphql({
+      await client.graphql({
         query: updateProgress,
         variables: {
           input: {
-            id: `${myRecord}`,
-            progress: `${newProgress}`,
+            id: myRecord,
+            progress: newProgress,
           },
         },
       });
     } catch (error) {
-      console.log("error is ", error);
+      console.error("Error updating progress:", error);
     }
   }
+
+  // Handler for radio button changes
   const onAnswerChange = (e, questionIndex) => {
-    setAnswers({
-      ...answers,
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
       [questionIndex]: e.target.value,
-    });
-  };
-  const calculateScore = (answers, module) => {
-    let score = 0;
-    // Loop through each question in the module's quizList
-    module.quizList.forEach((quiz, index) => {
-      // Check if the user's answer matches the correct answer
-      const correctAnswer = quiz.correctAnswer;
-      if (answers[index] === correctAnswer) {
-        score += 1; // Increment the score for each correct answer
-      }
-    });
-
-    return score;
+    }));
   };
 
+  // Function to calculate the total score
+  const calculateScore = () => {
+    return module.quizList.reduce((score, quiz, index) => {
+      return score + (answers[index] === quiz.correctAnswer ? 1 : 0);
+    }, 0);
+  };
+
+  // Function to handle quiz submission
   async function handleSubmit() {
-    //calculate the total score
-    const score = calculateScore(answers, module);
-    //if the total score matches the total question number, it will mark as pass
-    if (score >= module.scoreForPassing) {
-      if (!completedModules.includes(moduleName)) {
-        const newCompletedModules = [...completedModules, moduleName];
-        await updateOurProgress(JSON.stringify(newCompletedModules));
-        updateCompletedModules(newCompletedModules);
-      }
+    const score = calculateScore();
+    // Check if the score is sufficient for passing
+    if (
+      score >= module.scoreForPassing &&
+      !completedModules.includes(moduleName)
+    ) {
+      const newCompletedModules = [...completedModules, moduleName];
+      await updateOurProgress(JSON.stringify(newCompletedModules));
+      updateCompletedModules(newCompletedModules);
     }
-    navigate(`/result/${moduleName}`, { state: { module, answers, myRecord } }); // Navigate to your quiz page route
+    navigate(`/result/${moduleName}`, { state: { module, answers, myRecord } });
   }
 
   return (
@@ -79,27 +73,22 @@ const Quiz = (props) => {
       <NavBar />
       <div className="content-body quiz-body">
         <h1 className="header1">{moduleName} Quiz</h1>
-        {location.state.module.quizList.map((quizItem, index) => (
+        {module.quizList.map((quizItem, index) => (
           <Card key={index} className="margin-top-medium">
             <h3 className="header3">Question {index + 1}</h3>
             <p className="body-text-2 padding-vertical-medium">
               {quizItem.question}
             </p>
             <ConfigProvider
-              theme={{
-                token: {
-                  colorPrimary: "#9e2a2b",
-                  borderRadius: 2,
-                },
-              }}
+              theme={{ token: { colorPrimary: "#9e2a2b", borderRadius: 2 } }}
             >
               <Radio.Group onChange={(e) => onAnswerChange(e, index)}>
                 <Space direction="vertical">
                   {quizItem.options.map((option, optionIndex) => (
                     <Radio
-                      className="body-text-2 margin-top-small"
                       key={optionIndex}
                       value={option}
+                      className="body-text-2 margin-top-small"
                     >
                       {option}
                     </Radio>
@@ -109,7 +98,6 @@ const Quiz = (props) => {
             </ConfigProvider>
           </Card>
         ))}
-
         <div className="button-container">
           <Button className="action-button" onClick={handleSubmit} block>
             <div className="body-text-1">Submit</div>
